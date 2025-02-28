70e25d322698337130b9022690227f699feb68ba
Henry-YiW <kuaileywg@gmail.com>
2025-02-27 18:54:57 -0600

    add new feature

--- MP1/6DOF/network.py
+++ MP1/6DOF/network.py
@@ -73,7 +73,7 @@ class SimpleModel(nn.Module):
 
         # Stack into a valid 3×3 rotation matrix
         R = torch.stack((r1, r2, r3), dim=2)  # Shape: (batch_size, 3, 3)
-        logging.info(f'R: {R.shape}')
+        # logging.info(f'R: {R.shape}')
         return R
 
 
